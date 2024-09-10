4d4dce77d1eecc63b906cb380fc7c2acca331645
dil-bpizag <102038040+dil-bpizag@users.noreply.github.com>
2024-09-10 21:19:12 +0200

    update docker-compose.yml, cors fix

--- backend/order-service/app/main.py
+++ backend/order-service/app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from app.database import SessionLocal, init_database
 from typing import AsyncIterator
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.kafka import get_kafka_producer
 from app.routes import router
@@ -17,6 +18,13 @@ async def lifespan(app: FastAPI) -> AsyncIterator[None]:
     app.state.kafka_producer.close()
 
 app = FastAPI(lifespan=lifespan)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Adjust according to your needs
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 init_database()
 
--- backend/print-service/app/main.py
+++ backend/print-service/app/main.py
@@ -5,6 +5,7 @@ from typing import AsyncIterator
 from app.kafka import start_kafka_consumer, stop_kafka_consumer
 from app.print_service import start_batch_processor
 from app.routes import router
+from fastapi.middleware.cors import CORSMiddleware
 
 async def lifespan(app: FastAPI) -> AsyncIterator[None]:
     print("Starting up")
@@ -17,6 +18,13 @@ async def lifespan(app: FastAPI) -> AsyncIterator[None]:
         stop_kafka_consumer(app.state.consumer, app.state.consumer_thread)
 
 app = FastAPI(lifespan=lifespan)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Adjust according to your needs
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 init_database()
 
